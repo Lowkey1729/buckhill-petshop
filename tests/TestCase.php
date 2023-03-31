c9b6a00cf6df2581af60d98c8135dc4652277c56
lowkey1729 <olarewajumojeed9@gmail.com>
2023-03-31 02:02:51 +0100

    - New authentication for users and admins separately. - changed the hardcoded password for test authenticated

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,6 +13,37 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     use DatabaseMigrations;
 
+    public User|null $user;
+
+    /**
+     * @throws Throwable
+     */
+    public function getAdminAccessToken(): string
+    {
+        $user = User::factory()->create();
+        $user->update(['is_admin' => UserType::admin()->value]);
+        $response = $this->json('POST', route('admin.login'), [
+            'email' => $user->email,
+            'password' => "password",
+        ])->decodeResponseJson();
+
+        return $response['data']['token'];
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function getUserAccessToken(): string
+    {
+        $this->user = User::factory()->create();
+        $response = $this->json('POST', route('user.login'), [
+            'email' => $this->user->email,
+            'password' => "password",
+        ]);
+
+        return $response['data']['token'];
+    }
+
     /**
      * Reset the migrations
      */
@@ -20,20 +51,5 @@ abstract class TestCase extends BaseTestCase
     {
         $this->artisan('migrate:reset');
         parent::tearDown();
-    }
-
-    /**
-     * @throws Throwable
-     */
-    public function getAccessToken(): string
-    {
-        $user = User::query()->first();
-        $user?->update(['is_admin' => UserType::admin()->value]);
-        $response = $this->json('POST', route('admin.login'), [
-            'email' => $user?->email,
-            'password' => 1234,
-        ])->decodeResponseJson();
-
-        return $response['data']['token'];
     }
 }
