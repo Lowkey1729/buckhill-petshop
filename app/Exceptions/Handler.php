4ae719f9fd909428248f6961b273099bb84130ee
lowkey1729 <olarewajumojeed9@gmail.com>
2023-04-01 17:17:53 +0100

    Handled some common http exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Services\Helpers\ApiResponse;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Exceptions\InvalidSignatureException;
+use Psr\Log\LogLevel;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +17,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of exception types with their corresponding custom log levels.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     * @var array<class-string<\Throwable>, LogLevel::*>
      */
     protected $levels = [
         //
@@ -44,5 +51,38 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e): JsonResponse
+    {
+        return match (true) {
+            $e instanceof MethodNotAllowedHttpException => $this->httpResponseException(
+                $e->getMessage(),
+                405
+            ),
+            $e instanceof NotFoundHttpException => $this->httpResponseException(
+                'Api resource not found',
+                httpStatusCode: 404
+            ),
+            $e instanceof InvalidSignatureException => $this->httpResponseException(
+                'Invalid signature',
+                httpStatusCode: 400
+            ),
+            $e instanceof AuthenticationException => $this->httpResponseException(
+                'Not authenticated',
+                httpStatusCode: 401
+            )
+        };
+    }
+
+    protected function httpResponseException(
+        string $message,
+        int    $httpStatusCode
+    ): JsonResponse {
+        return
+            ApiResponse::failed(
+                $message,
+                httpStatusCode: $httpStatusCode
+            );
     }
 }
