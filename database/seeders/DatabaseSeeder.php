b2a2c8e9d0a317f16b69c164b824cf5a47ef9574
lowkey1729 <olarewajumojeed9@gmail.com>
2023-04-05 17:46:25 +0100

    Included Admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             FileSeeder::class,
             PostSeeder::class,
             PromotionSeeder::class,
+            AdminSeeder::class
         ]);
     }
 }
