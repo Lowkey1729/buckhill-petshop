9bec9e03cc6ec395a3e61692241ba3e4f1d4dd00
lowkey1729 <olarewajumojeed9@gmail.com>
2023-04-05 17:45:51 +0100

    Modified the number of acceptable characters for password

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -26,7 +26,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => ['required', 'string', 'email:rfc', 'max:255'],
-            'password' => ['required', 'string', 'min:8']
+            'password' => ['required', 'string']
         ];
     }
 }
